a6b70e79590da17d6a0d585f4cedd4004b0c1926
dfichev <fichev@gmail.com>
2024-03-27 11:06:51 +0200

    a

---
+++ FundamentalsModule/12.ArraysExercise/01.Train/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+            int total = 0;
+            int[] people = new int[wagons];
+
+            for (int i = 0; i < wagons; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+                total += people[i];
+            }
+            for (int i = 0; i < wagons; i++)
+            {
+                Console.Write($"{people[i]} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ FundamentalsModule/12.ArraysExercise/02.CommonElements/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] firstArray = Console.ReadLine()
+    .Split()
+    .ToArray();
+string[] secondArray = Console.ReadLine()
+    .Split()
+    .ToArray();
+
+for (int i = 0; i < firstArray.Length; i++)
+{
+    for (int j = 0; j < secondArray.Length; j++)
+    {
+        if (firstArray[i] == secondArray[j])
+        {
+            Console.Write($"{firstArray[i]} ");
+        }
+
+    }
+}
