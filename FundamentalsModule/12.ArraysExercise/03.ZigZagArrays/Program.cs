146842f4b25d0d8e31851f278439c08038e8528f
dfichev <fichev@gmail.com>
2024-03-29 13:03:41 +0200

    feed

---
+++ FundamentalsModule/12.ArraysExercise/03.ZigZagArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _03.ZigZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] first = new int[n];
+            int[] second = new int[n];
+            int j = 0;
+            int k = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] pairs = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i % 2 == 0)
+                {
+                    first[k] = pairs[0];
+                    k++;
+                    second[j] = pairs[1];
+                    j++;
+                }
+                else
+                {
+                    first[k] = pairs[1];
+                    k++;
+                    second[j] = pairs[0];
+                    j++;
+                }
+            }
+            Console.WriteLine(string.Join(" ", first));
+            Console.WriteLine(string.Join(" ", second));
+        }
+    }
+}
---
+++ FundamentalsModule/12.ArraysExercise/04.ArrayRotation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            //OPERATIONS
+
+            for (int i = 0; i < rotations; i++)
+            {
+                
+                string firstElement = input[0];
+
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+                    input[j] = input[j + 1];
+
+                }
+                input[input.Length - 1] = firstElement;
+            }
+            //OUTPUT
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
