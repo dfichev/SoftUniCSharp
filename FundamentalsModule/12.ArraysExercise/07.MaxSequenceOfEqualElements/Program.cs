841e23ac0daba0bcef25cd496f2ccea7601f8a53
dfichev <fichev@gmail.com>
2024-03-30 14:17:14 +0200

    feed

---
+++ FundamentalsModule/12.ArraysExercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _07.MaxSequenceOfEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            int counter = 1;
+            int maxCounter = 0;
+            int num = 0;
+
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                for (int j = i + 1; j < sequence.Length; j++)
+                {
+                        counter++;
+
+                    if (sequence[i] == sequence[j])
+                    {
+
+                        if (maxCounter < counter)
+                        {
+                            maxCounter = counter;
+                            num = sequence[i];
+                        }
+
+                    }
+                    else
+                    {
+                        counter = 1;
+                        break;
+                    }
+                }
+                counter = 1;
+            }
+            for (int k = 0; k < maxCounter; k++)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
