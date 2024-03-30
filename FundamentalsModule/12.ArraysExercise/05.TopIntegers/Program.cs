3a98fa4c7a4316e1e664800fd3160507bc42ad44
dfichev <fichev@gmail.com>
2024-03-30 14:16:14 +0200

    feed

---
+++ FundamentalsModule/12.ArraysExercise/05.TopIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isTop = true;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if (isTop)
+                {
+                    // OUTPUT
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+        
+        }
+    }
+}
---
+++ FundamentalsModule/12.ArraysExercise/06.EqualSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _06.EqualSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+            bool no = default;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+                
+                for (int j = sequence.Length - 1; j > i; j--)
+                {
+                    rightSum += sequence[j];
+
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    leftSum += sequence[k];
+
+                }
+                if (i == 0)
+                {
+                    leftSum = 0;
+                }
+                else if (i == sequence.Length - 1)
+                {
+                    rightSum = 0;
+                }
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine($"{i}");
+                    no = true;
+                }
+            }
+            if (no == false)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
