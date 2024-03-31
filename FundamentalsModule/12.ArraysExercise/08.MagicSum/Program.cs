3c90a092dd852728a844575cd114daba2a12f295
dfichev <fichev@gmail.com>
2024-03-31 14:26:47 +0300

    feed

---
+++ FundamentalsModule/12.ArraysExercise/08.MagicSum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _08.MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            int uniqueNum = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 0;
+
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                for (int j = i + 1; j < sequence.Length; j++)
+                {
+                    if (sequence[i] + sequence[j] == uniqueNum)
+                    {
+                        a = sequence[i];
+                        b = sequence[j];
+                        Console.WriteLine($"{a} {b}");
+                    }
+                }
+            }
+
+        }
+    }
+}
