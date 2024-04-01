9b06aa735f55e4b67d772dd5fb922db6cff8b4b1
dfichev <fichev@gmail.com>
2024-04-01 19:01:27 +0300

    feed

---
+++ FundamentalsModule/15.MethodsExercise/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace _01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            if (first > second) 
+            { 
+                char swap = first;
+                first = second;
+                second = swap;
+            }
+
+            PrintCharRange(first, second);
+        }
+
+        static void PrintCharRange(char first, char second)
+        {
+            int from = (int)first;
+            int to = (int)second;
+
+            for (int i = from + 1; i < to; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+            
+        }
+
+    }
+}
