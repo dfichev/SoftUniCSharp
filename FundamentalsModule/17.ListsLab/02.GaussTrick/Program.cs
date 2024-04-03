ba1d3d4c1a9a939814eced3993e872bede215b93
dfichev <fichev@gmail.com>
2024-04-03 17:47:51 +0300

    feed

---
+++ FundamentalsModule/17.ListsLab/02.GaussTrick/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                int first = list[i];
+                int last = list[list.Count - i - 1];
+                newList.Add(first + last);
+            }
+            if (list.Count % 2 == 1)
+            {
+                newList.Add(list[list.Count / 2]);
+            }
+            Console.WriteLine(string.Join(" ", newList));
+        }
+    }
+}
