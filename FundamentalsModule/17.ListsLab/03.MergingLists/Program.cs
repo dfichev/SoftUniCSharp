ebce5e346a7fcd45ddee098ea6bae0632b4be8f8
dfichev <fichev@gmail.com>
2024-04-08 18:57:57 +0300

    feed

---
+++ FundamentalsModule/17.ListsLab/03.MergingLists/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> list2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> combined = new List<int>();
+
+            int iterations = Math.Max(list1.Count, list2.Count);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (i < list1.Count)
+                {
+                    combined.Add(list1[i]);
+                }
+                if (i < list2.Count)
+                {
+                    combined.Add(list2[i]);
+
+                }
+            }
+            Console.WriteLine(string.Join(" ", combined));
+        }
+    }
+}
---
+++ FundamentalsModule/17.ListsLab/04.ListOfProducts/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _04.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+
+        }
+    }
+}
